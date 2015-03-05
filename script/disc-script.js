d4773483326205727dc12535b0f7b3116ae40481
Suman Barick <codotronix@gmail.com>
2015-03-05 17:23:48 +0530

    true initial commit
    1st working copy of this project

---
+++ script/disc-script.js
@@ -0,0 +1,45 @@
+$(function () {
+	function manageCircularDisc () {
+		var winHeight = $(window).height();
+		var winWidth = $(window).width();
+
+		$('#disc').css({
+			'left': (winWidth - $('#disc').width())/2 ,
+			'top': (winHeight - $('#disc').height())/2
+		});
+
+		$('#center-disc').css({
+			'left': ($('#disc').width() - $('#center-disc').width())/2 ,
+			'top': ($('#disc').height() - $('#center-disc').height())/2 - 45
+		});
+
+		var cx = winWidth/2;
+		var cy = winHeight/2;
+	 	var r = $('#disc').width() / 2;
+
+		var smallDiscId = '#small-disc-';
+		var degree = 0;
+		var discX = 0;
+		var discY = 0;
+		//position all 6 small discs
+		for(var i=1; i<=6; i++) {
+			degree = i*60;
+			discX = cx + r * Math.cos(degree * Math.PI/180) - (winWidth - $('#disc').width())/2 - 60;
+			discY = cy + r * Math.sin(degree * Math.PI/180) - (winHeight - $('#disc').height())/2 - 60;
+			$(smallDiscId+i).css({
+				'top': discY,
+				'left': discX 
+			});
+		}
+	}
+
+	manageCircularDisc();
+
+	$(window).resize(function () {
+		manageCircularDisc();
+	});
+
+	$('#center-disc').click(function () {
+		$(this).toggleClass('center-disc-bg2');
+	});
+});
---
+++ script/sky-script.js
@@ -0,0 +1,111 @@
+$(function () {
+	"use strict"
+	//console.log('ready');
+	//this is our array of stars
+	var stars = [];
+	var winHeight = $(window).height();
+	var winWidth = $(window).width();
+	var noOfStar = 2000;
+	$('#canvasBlack')[0].width = winWidth;
+	$('#canvasBlack')[0].height = winHeight;
+	/*$('#canvasBlack').css({
+		'width': winWidth,
+		'height': winHeight,
+		'background': '#000'
+	});*/
+	var ctx = $('#canvasBlack')[0].getContext('2d');
+	ctx.fillStyle = "#FFF";
+
+	//lets create noOfStar stars
+	for(var i=0; i<noOfStar; i++) {
+		stars[i] = {};
+		stars[i].x = Math.floor(Math.random() * winWidth);
+		//stars[i].x = winWidth/2 - 100 + Math.floor(Math.random() * 200);
+		stars[i].y = Math.floor(Math.random() * winHeight);
+		//stars[i].y = winHeight/2 - 100 + Math.floor(Math.random() * 200);
+		stars[i].size = Math.random() + .3;
+		stars[i].speed = Math.random() * 2;
+		stars[i].direction = Math.floor(Math.random() * 2) + 1; // 1=X axis, 2=Y axis
+		
+		if (stars[i].direction == 1) { 			//change in x axis
+			if (stars[i].x <= winWidth/2) {
+				stars[i].posNeg = 1; 
+			} else {
+				stars[i].posNeg = 2;
+			}
+		} else { 								//change in y-axis
+			if (stars[i].y <= winHeight/2) {
+				stars[i].posNeg = 1; 
+			} else {
+				stars[i].posNeg = 2;
+			}
+		}
+	}
+	
+	function clearCanvas () {
+		//Clear the canvas
+		ctx.clearRect(0,0,winWidth,winHeight);
+	}
+
+	//Draw all the Stars
+	function drawStars () {
+		for(var i=0; i<noOfStar; i++) {
+			ctx.fillRect(stars[i].x,stars[i].y,stars[i].size,stars[i].size);
+		}
+	}
+
+	function moveStars () {		
+		for(var i = 0; i < noOfStar; i++) {
+			/* Change position for next cycle */
+			if (stars[i].direction == 1 && stars[i].posNeg == 1) {
+				stars[i].x -= stars[i].speed;
+			} else if (stars[i].direction == 1 && stars[i].posNeg == 2) {
+				stars[i].x += stars[i].speed;
+			} else if (stars[i].direction == 2 && stars[i].posNeg == 1) {
+				stars[i].y -= stars[i].speed;
+			} else if (stars[i].direction == 2 && stars[i].posNeg == 2) {
+				stars[i].y += stars[i].speed;
+			}
+
+
+			//check out of boundary conditions
+			if (stars[i].x > winWidth || stars[i].x < 0) { 
+				//stars[i].x = winWidth/2 - 200 + Math.floor(Math.random() * 400);
+				//stars[i].y = winHeight/2 - 200 + Math.floor(Math.random() * 400);	
+				stars[i].x = Math.floor(Math.random() * winWidth);
+				stars[i].y = Math.floor(Math.random() * winHeight);				
+			}
+
+			if (stars[i].y > winHeight || stars[i].y < 0) { 
+				//stars[i].x = winWidth/2 - 200 + Math.floor(Math.random() * 400);
+				//stars[i].y = winHeight/2 - 200 + Math.floor(Math.random() * 400);
+				stars[i].x = Math.floor(Math.random() * winWidth);
+				stars[i].y = Math.floor(Math.random() * winHeight);
+			}
+			/* Change direction complete */				
+			if (stars[i].direction == 1) { 			//change in x axis
+				if (stars[i].x <= winWidth/2) {
+					stars[i].posNeg = 1; 
+				} else {
+					stars[i].posNeg = 2;
+				}
+			} else { 								//change in y-axis
+				if (stars[i].y <= winHeight/2) {
+					stars[i].posNeg = 1; 
+				} else {
+					stars[i].posNeg = 2;
+				}
+			}
+		}		
+	}
+
+	function wakeTheSky () {
+		clearCanvas();
+		drawStars();
+		moveStars();
+		window.setTimeout(wakeTheSky, 100);
+	}
+
+	wakeTheSky();
+
+});
