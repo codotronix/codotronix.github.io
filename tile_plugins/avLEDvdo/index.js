8b3e67310db164581ce98e77c682a8ee722b31a3
Suman Barick <codotronix@gmail.com>
2019-07-23 11:54:53 +0530

    Update index.js

--- tile_plugins/avLEDvdo/index.js
+++ tile_plugins/avLEDvdo/index.js
@@ -4,7 +4,7 @@
     //the container
     var pluginContainer = $('#avLEDvdo .plugin');
 
-    var h = '<iframe width="100%" height="auto" src="https://www.youtube.com/embed/w7I0x2ntyys" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    var h = '<iframe width="100%" height="90%" src="https://www.youtube.com/embed/w7I0x2ntyys" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     
     pluginContainer.html(h);
 })()
