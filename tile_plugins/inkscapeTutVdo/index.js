42478da24708afdf3a6b84f18a23549153f9b5c6
Suman Barick <codotronix@gmail.com>
2019-07-23 11:55:12 +0530

    Update index.js

--- tile_plugins/inkscapeTutVdo/index.js
+++ tile_plugins/inkscapeTutVdo/index.js
@@ -4,7 +4,7 @@
     //the container
     var pluginContainer = $('#inkscapeTutVdo .plugin');
 
-    var h = '<iframe width="100%" height="auto" src="https://www.youtube.com/embed/osxtZzpV3W0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    var h = '<iframe width="100%" height="90%" src="https://www.youtube.com/embed/osxtZzpV3W0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     
     pluginContainer.html(h);
 })()
